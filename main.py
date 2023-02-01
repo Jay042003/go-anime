f53c446484287841a9f4044b47fc63925103da85
Sanmeet007 <ssanmeet123@gmail.com>
2023-02-01 17:55:17 +0530

    changed main file from jay to main

---
+++ main.py
@@ -0,0 +1,17 @@
+from utils.cliargparser import CliArguments
+from utils.scrapper import Scrapper
+import os
+
+
+#################################################################
+WEBDRIVER_PATH = "C:/webdrivers/chromedriver.exe"
+WEBDRIVER_PATH = os.environ.get("WEBDRIVER_PATH", WEBDRIVER_PATH)
+
+if __name__ == "__main__":
+    scrapper = Scrapper(
+        wdpath=WEBDRIVER_PATH,  # passing webdriver path
+        cli_args=CliArguments().todict(),  # passing cli arguments as dict
+    )
+
+    # Start scrapping
+    scrapper.init()
