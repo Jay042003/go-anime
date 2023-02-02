9d9979c36838de2ea2ecb8caf4e7d9a6c2eb7250
jay <kadejay666@gmail.com>
2023-02-02 23:40:17 +0530

    base code done

--- utils/scrapper.py
+++ utils/scrapper.py
@@ -1,64 +1,80 @@
 from selenium import webdriver
-from selenium.webdriver.remote.command import Command
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time, sys
 
 
 class EpisodeQuality:
-    low = "360p"
-    mid = "720p"
-    high = "1080p"
+    def low(self):
+        return -3
+
+    def mid(self):
+        return -2
+
+    def mid(self):
+        return -1
+
+
+quality = EpisodeQuality()
+start_from = 1
+max_episodes = 12
 
 
 class Scrapper:
     def __init__(self, wdpath: str, cli_args: dict[str, str]) -> None:
         self.driver: webdriver.Chrome = webdriver.Chrome(wdpath)
+        self.d_links = []
+        self.v_links = []
 
     def init(self) -> None:
         try:
-            self.driver.get(
-                'https://animepahe.com/anime/c9a03d10-964a-bd20-ee4d-8370be493f28')
+            for i in range(start_from, max_episodes):
+                self.driver.get(
+                    'https://animepahe.com/play/c9a03d10-964a-bd20-ee4d-8370be493f28/57fadf8ec9a7ad770e7a56c872b76b5024cd8cf56d1e02cce2d8d975f1a18e1f')
 
-            self.driver.execute_cdp_cmd(
-                "Network.setUserAgentOverride",
-                {
-                    "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36"
-                },
-            )
+                self.driver.execute_cdp_cmd(
+                    "Network.setUserAgentOverride",
+                    {
+                        "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36"
+                    },
+                )
 
-            # arranging page in ascending order
-            self.driver.execute_script("""
-             document.querySelector("body > section > article > div.content-wrapper > div.episode-bar.form-group.row > div.col-6.bar > div > label:nth-child(1)").click()
-            """
-                                  )
-            # going to the page one
-            time.sleep(2)
-            self.driver.execute_script("""
-             document.querySelector("body > section > article > div.content-wrapper > div.episode-list-wrapper > div > div:nth-child(1) > div > div.episode-snapshot > a").click()
-            """
-                                  )
-
-            soup = BeautifulSoup(self.driver.page_source, "lxml")
-            self.driver.get(soup.find_all("a", class_="dropdown-item")[-2]["href"])
-
-            time.sleep(4)
-
-            soup2 = BeautifulSoup(self.driver.page_source, "lxml")
-            self.driver.get(
-                soup2.find("a", class_="btn btn-primary btn-block redirect")["href"]
-            )
-
-            # Executing js script to start downloading
-            self.driver.execute_script(
+                # selecting ith episode
+                self.driver.execute_script(f"""
+                document.querySelector("#scrollArea > a:nth-child({i})").click()
                 """
-                document.querySelector("form").submit()
-            """
-            )
+                                           )
+
+                # waiting for links to load
+                time.sleep(1)
+
+                #  visiting the redirect page
+                soup = BeautifulSoup(self.driver.page_source, "lxml")
+                self.driver.get(soup.find_all("a", class_="dropdown-item")[quality.low()]["href"])
+
+                time.sleep(4)
+
+                # getting dowload links
+                soup2 = BeautifulSoup(self.driver.page_source, "lxml")
+                self.d_links.append(
+                    soup2.find("a", class_="btn btn-primary btn-block redirect")["href"]
+                )          
+
+                # preparing for the next link
+                self.driver.back()  
+                self.driver.back()  
+
+            # downloading from all the links given to the scraper
+            for i in self.d_links:
+
+                self.driver.get(i)
+
+                # Executing js script to start downloading
+                self.driver.execute_script(
+                    """
+                    document.querySelector("form").submit()
+                """
+                )
+                time.sleep(1)
 
             print("Please quit after downloading is complete")
 
@@ -71,5 +87,5 @@ class Scrapper:
             sys.exit()
 
         except Exception as E:
-            print("\nProcess exited due to an error")
+            print(f"\nProcess exited due to an error: {E}")
             sys.exit()
